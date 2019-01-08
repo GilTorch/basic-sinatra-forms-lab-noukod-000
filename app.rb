14656020434ab74c2e2691304ee211d9bf670b46
Gilbert Torchon <gilbert@noukod.com>
2019-01-08 20:22:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class App < Sinatra::Base
   get '/newteam' do
     erb :newteam
   end
+
+  get '/team' do
+    @team_name=@params[:team_name]
+    @coach=@params[:coach]
+    @point_guard=@params[:point_guard]
+    @shooting_guard=@params[:shooting_guard]
+    @power_forward=@params[:power_forward]
+    @small_forward=@params[:small_forward]
+    @center=@params[:center]
+    erb :team 
+  end
+  
 end
